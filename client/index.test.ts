a195481dfeacee07139bae329429bfb07334cd11
Anuj <ok.anuj30@gmail.com>
2025-06-21 17:06:37 +0530

    written client-test code

---
+++ client/index.test.ts
@@ -0,0 +1,37 @@
+import {test,expect} from "bun:test";
+import { LiteSVM } from "litesvm";
+import { Buffer } from "buffer";
+import {
+	PublicKey,
+	Transaction,
+	SystemProgram,
+	Keypair,
+	LAMPORTS_PER_SOL,
+	TransactionInstruction,
+} from "@solana/web3.js";
+
+
+test("one transfer", () => {
+	const svm = new LiteSVM();
+	const payer = Keypair.generate();
+	svm.airdrop(payer.publicKey, BigInt(LAMPORTS_PER_SOL));
+    const programId = Keypair.generate();
+    svm.addProgramFromFile(programId.publicKey,"../target/contract.so");
+    const seeds = [Buffer.from("client"), payer.publicKey.toBuffer()];
+    const [pda,bump]=PublicKey.findProgramAddressSync(seeds,programId.publicKey);
+	const ixs = new TransactionInstruction({
+        keys:[
+            {pubkey: payer.publicKey, isSigner:true,isWritable:true},
+            {pubkey: pda, isSigner:false,isWritable:true},
+            {pubkey: SystemProgram.programId, isSigner:false,isWritable:false}
+        ],
+        programId: programId.publicKey,
+        data:Buffer.from(""),
+    })
+	const tx = new Transaction();
+	tx.recentBlockhash = svm.latestBlockhash();
+	tx.add(ixs);
+    tx.feePayer=payer.publicKey;
+	tx.sign(payer);
+	svm.sendTransaction(tx);
+});
